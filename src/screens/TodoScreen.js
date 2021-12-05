0efc528e53e3388aabaf379a1e6378e93c00bf9c
Alexandra Khutorna <khutornaalexandra@gmail.com>
2021-12-05 12:11:00 +0200

    added theme and window confirm

--- src/screens/TodoScreen.js
+++ src/screens/TodoScreen.js
@@ -1,13 +1,45 @@
 import React from 'react'
 import { StyleSheet, View, Text, Button } from 'react-native'
+import { THEME } from '../theme'
+import { AppCard } from '../components/ui/AppCard'
 
-export const TodoScreen = ({ goBack, todo }) => {
+export const TodoScreen = ({ goBack, todo, onRemove }) => {
   return (
     <View>
-      <Text>{todo.title}</Text>
-      <Button title='Назад' onPress={goBack} />
+      <AppCard style={styles.card}>
+        <Text style={styles.title}>{todo.title}</Text>
+        <Button title='Ред.' />
+      </AppCard>
+
+      <View style={styles.buttons}>
+        <View style={styles.button}>
+          <Button title='Назад' onPress={goBack} color={THEME.GREY_COLOR} />
+        </View>
+        <View style={styles.button}>
+          <Button
+            title='Удалить'
+            color={THEME.DANGER_COLOR}
+            onPress={() => onRemove(todo.id)}
+          />
+        </View>
+      </View>
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  buttons: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  card: {
+    marginBottom: 20,
+    padding: 15
+  },
+  button: {
+    width: '40%'
+  },
+  title: {
+    fontSize: 20
+  }
+})
